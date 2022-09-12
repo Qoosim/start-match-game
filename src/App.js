56b5916418cf6ce310b56e1fcc5b60ece6e98875
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-09-12 16:47:44 +0000

    Refactor the start and button tag to use loop instead of multiple div and button tags

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import StarMatch from './components/StarMatch';
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello world</h2>
+      <StarMatch />
     </div>
   );
 }
