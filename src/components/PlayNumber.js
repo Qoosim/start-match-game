6300db0d7e013b14b9db2f0727dd379a7146dab2
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-09-13 00:05:27 +0000

    Decompose StarMatch component to several components

---
+++ src/components/PlayNumber.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const PlayNumber = (props) => {
+  return (
+    <button
+      className="number"
+      onClick={() => console.log('Num', props.number)}
+    >
+      {props.number}
+    </button>
+  )
+}
+
+export default PlayNumber;
